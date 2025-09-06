5aafb0eedda0b0eca2afac0bcbedfccba38d486c
hyzboy <hyzboy@gmail.com>
2025-09-06 12:40:36 +0800

    丰富Pipe通信

--- src/Posix/Pipe.cpp
+++ src/Posix/Pipe.cpp
@@ -1,28 +1,32 @@
 #include<hgl/proc/Pipe.h>
+#include<unistd.h>
+#include<errno.h>
 
 namespace hgl
 {
     bool CreatePipe(pipe_pair &pp)
     {
-        SECURITY_ATTRIBUTES sa;
-
-        sa.nLength=sizeof(SECURITY_ATTRIBUTES);
-        sa.bInheritHandle=TRUE;
-        sa.lpSecurityDescriptor=nullptr;
-
-        return ::CreatePipe(&(pp[0]),&(pp[1]),&sa,0);
+        int fds[2];
+        if(pipe(fds)==0)
+        {
+            pp[0]=fds[0];
+            pp[1]=fds[1];
+            return true;
+        }
+        pp[0]=PIPE_NULL;pp[1]=PIPE_NULL;
+        return false;
     }
 
     void ClosePipe(pipe_pair &pp)
     {
         if(pp[0]!=PIPE_NULL)
         {
-            CloseHandle(pp[0]);
+            close(pp[0]);
             pp[0]=PIPE_NULL;
         }
         if(pp[1]!=PIPE_NULL)
         {
-            CloseHandle(pp[1]);
+            close(pp[1]);
             pp[1]=PIPE_NULL;
         }
     }
@@ -30,25 +34,23 @@ namespace hgl
     void ClosePipe(pipe_ptr h)
     {
         if(h!=PIPE_NULL)
-            CloseHandle(h);
+            close(h);
     }
 
     int64 ReadPipe(pipe_ptr h, void *buf, int64 size)
     {
         if(h==PIPE_NULL || !buf || size<=0) return -1;
-        DWORD read_bytes=0;
-        if(!ReadFile(h, buf, (DWORD)size, &read_bytes, nullptr))
-            return -1;
-        return (int64)read_bytes;
+        ssize_t r = read(h, buf, (size_t)size);
+        if(r<0) return -1;
+        return (int64)r;
     }
 
     int64 WritePipe(pipe_ptr h, const void *buf, int64 size)
     {
         if(h==PIPE_NULL || !buf || size<=0) return -1;
-        DWORD written=0;
-        if(!WriteFile(h, buf, (DWORD)size, &written, nullptr))
-            return -1;
-        return (int64)written;
+        ssize_t w = write(h, buf, (size_t)size);
+        if(w<0) return -1;
+        return (int64)w;
     }
 
     int64 SkipPipe(pipe_ptr h, int64 size)
@@ -60,11 +62,10 @@ namespace hgl
         while(remain>0)
         {
             int64 toread = (remain>buf_sz)?buf_sz:remain;
-            int64 r = ReadPipe(h, tmp, toread);
+            ssize_t r = read(h, tmp, (size_t)toread);
             if(r<=0) break;
             remain -= r;
         }
         return size - remain;
     }
-}//namespace hgl
-
+}
